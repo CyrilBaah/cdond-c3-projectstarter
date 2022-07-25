d99ee53433022694af8b3e9ced803b287cf7bd41
CyrilBaah <cyril.baah@amalitech.org>
2022-07-25 08:12:41 +0000

    fixing frontend smoke test return error

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,13 @@
-URL="http://udapeople-${CIRCLE_WORKFLOW_ID:0:7}.s3-website-us-east-1.amazonaws.com/#/employees"            
-echo ${URL} 
-if curl -s ${URL} | grep "Welcome"
-then
-    # Change this to 0 after the job fails
-    return 0
-else
-    return 1
-fi
+#!/bin/bash
+function a(){
+    URL="http://udapeople-${CIRCLE_WORKFLOW_ID:0:7}.s3-website-us-east-1.amazonaws.com/#/employees"
+    echo ${URL}
+    if curl -s ${URL} | grep "Welcome"; then
+        #Change this to 0 after the job fails
+        return 0
+    else
+        return 1
+    fi
+}
+
+a
